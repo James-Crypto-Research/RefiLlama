12ce2f2940fe2e4e346986ae408f3bdd5615301e
James Chapman <james@paymenteconomics.com>
2022-05-02 14:05:43 -0400

    Added two new functions

---
+++ R/get_chain_tvl.R
@@ -0,0 +1,11 @@
+# This function gets the historical TVL for a given chain
+
+get_chain_tvl <- function(chain="Ethereum"){
+  end_point <- glue::glue("charts/", chain)
+  x <- call_defillama_api(end_point)
+  x <- jsonlite::fromJSON(x) |>
+    tibble::as_tibble() |>
+    mutate(date = as.Date(as.POSIXct(as.numeric(date),origin="1970-01-01 00:00:00")))
+  return(x)
+}
+
---
+++ R/get_protocol_list.R
@@ -0,0 +1,19 @@
+# These functions grab all the protocol information
+
+
+get_protocol_list <- function(tvl_limit = 0, which_chain = NULL, details=FALSE){
+  resp <- call_defillama_api("protocols") |> jsonlite::fromJSON()
+  tmp <- resp |> filter(tvl >= tvl_limit) |> select(name,chain, chains,tvl) |>
+    tibble::as_tibble()
+  if (!is.null(which_chain)) {
+    output <- tmp |> dplyr::filter(chain == which_chain )
+  } else {
+    output <- tmp
+  }
+  if (details) {
+    output$n_chains <- map(output$chains,length) |> unlist()
+  } else {
+    output <- output |> dplyr::select(name)
+  }
+  return(output)
+}
