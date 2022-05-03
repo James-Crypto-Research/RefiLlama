c5ef547c9ec1a07d5b1469049fe1bb06751241f9
James Chapman <james@paymenteconomics.com>
2022-05-03 19:47:43 -0400

    Added category
    Added the ability to get categories for protocols

--- R/get_protocol_list.R
+++ R/get_protocol_list.R
@@ -3,7 +3,7 @@
 
 get_protocol_list <- function(tvl_limit = 0, which_chain = NULL, details=FALSE){
   resp <- call_defillama_api("protocols") |> jsonlite::fromJSON()
-  tmp <- resp |> dplyr::filter(tvl >= tvl_limit) |> dplyr::select(name,chain, chains,tvl) |>
+  tmp <- resp |> dplyr::filter(tvl >= tvl_limit) |> dplyr::select(name,chain, chains,tvl,category) |>
     tibble::as_tibble()
   if (!is.null(which_chain)) {
     output <- tmp |> dplyr::filter(chain == which_chain )
@@ -12,6 +12,7 @@ get_protocol_list <- function(tvl_limit = 0, which_chain = NULL, details=FALSE){
   }
   if (details) {
     output$n_chains <- purrr::map(output$chains,length) |> unlist()
+    output$category <- output$category
   } else {
     output <- output |> dplyr::select(name)
   }
--- R/get_protocol_tvl.R
+++ R/get_protocol_tvl.R
@@ -1,6 +1,6 @@
 # This function grabs the TVL for a given protocol
 
-get_protocol_tvl <- function(protocol="MakerDAO"){
+get_protocol_tvl <- function(protocol="MakerDAO",type=FALSE){
   end_point <- glue::glue("protocol/",protocol)
   resp <- call_defillama_api(end_point) |> jsonlite::fromJSON()
   the_chains <- resp$chains
@@ -15,5 +15,9 @@ get_protocol_tvl <- function(protocol="MakerDAO"){
 
   tmp_data <- the_chains |> purrr::map(tmp_function) |>
     plyr::join_all(by="date") |> tibble::as_tibble()
+  if (type){
+    tmp_data$type <- resp[["category"]]
+    tmp_data <- tmp_data |> dplyr::relocate(type,.after=date)
+  }
   return(tmp_data)
 }
